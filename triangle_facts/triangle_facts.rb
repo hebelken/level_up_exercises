a1ed50f3b9c43dac37507552d35562f6f922e221
Ken Hebel <khebel@enova.com>
2015-05-28 11:52:05 -0500

    implementing feedback

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,19 +1,5 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-  attr_accessor :side1
-  attr_accessor :side2
-  attr_accessor :side3
-  attr_accessor :angle_a
-  attr_accessor :angle_b
-  attr_accessor :angle_c
-
-  protected :side1
-  protected :side2
-  protected :side3
-  protected :angle_a
-  protected :angle_b
-  protected :angle_c
-
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
@@ -23,16 +9,14 @@ class Triangle
     @angle_c = determine_angle(@side1, @side2, @side3)
   end
 
-  def law_of_cosines(a, b, c)
-    (a**2 + b**2 - c**2) / (2.0 * a * b)
-  end
-
   def radians_to_degrees(rads)
     (rads * 180 / Math::PI).round
   end
 
   def determine_angle(a, b, c)
-    radians_to_degrees(Math.acos(law_of_cosines(a, b, c)))
+    law_of_cosines = (a**2 + b**2 - c**2) / (2.0 * a * b)
+    rads = Math.acos(law_of_cosines)
+    radians_to_degrees(rads)
   end
 
   def equilateral?
@@ -44,19 +28,11 @@ class Triangle
   end
 
   def scalene?
-    if !(equilateral? || isosceles?)
-      true
-    else
-      false
-    end
+    !(equilateral? || isosceles?)
   end
 
   def right?
-    if (@angle_a || @angle_b || @angle_c) == 90
-      true
-    else
-      false
-    end
+    [@angle_a, @angle_b, @angle_c].include? 90
   end
 
   def recite_facts
@@ -65,7 +41,7 @@ class Triangle
       "hard to type." if isosceles?
     puts "This triangle is scalene and mathematically boring." if scalene?
 
-    puts "The angles of this triangle are #{angle_a},#{angle_b},#{angle_c}"
+    puts "The angles of this triangle are #{@angle_a},#{@angle_b},#{@angle_c}"
 
     puts "This triangle is also a right triangle!" if right?
     puts ""
