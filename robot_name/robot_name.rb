85d029592f38ce82db3350679d3e13b379bc62a0
Ken Hebel <khebel@enova.com>
2015-05-28 15:41:51 -0500

    Added RobotDirectory Class instead of mutating Class variable, Split up functionality between the two classes

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,41 +1,55 @@
+class NameCollisionError < RuntimeError; end
+class NameStructureError < RuntimeError; end
+
 class Robot
   attr_reader :name
 
   def initialize(args = {})
-    @@registry ||= []
-    self.name = args[:name_generator] ? args[:name_generator] : generate_name
-    @@registry << @name
-  end
-
-  def name=(value)
-    @name = value if check_duplicate?(value)
-  end
-
-  def self.registry
-    @@registry
-  end
-
-  def check_duplicate?(str)
-    if !@@registry.include?(str)
-      true
-    else
-      puts "A robot named '#{str}' already exists. Skipping robot."
-      return false
-    end
+    @name = ""
+    generate_name
+    @name = args[:name_generator].call unless !args[:name_generator]
+    name_structured_corectly?
   end
 
   def generate_name
-    generate_char = -> { ('A'..'Z').to_a.sample }
-    generate_num = -> { rand(10) }
+    generated_chars = 2.times { @name << ('A'..'Z').to_a.sample }
+    generate_num = 3.times { @name << ('0'..'9').to_a.sample }
+    @name
+  end
 
-    2.times.map { generate_char.call }.join +
-      3.times.map { generate_num.call }.join
+  def name_structured_corectly?
+    if !@name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+      raise NameStructureError, "Robot name must follow convention: AA111"
+    end
+    true
   end
 end
 
-50.times do
-  robot = Robot.new
-  puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
+class RobotDirectory
+  def initialize
+    @directory = []
+  end
+
+  def directory
+    @directory
+  end
+
+  def register(robot)
+    @directory << robot.name unless name_duplicated?(robot)
+  end
+
+  def name_duplicated?(robot)
+    if @directory.include?(robot.name)
+      raise NameCollisionError, "A robot named '#{robot.name}' already exists."
+    end
+    false
+  end
 end
 
-puts Robot.registry
+robodir = RobotDirectory.new
+robot = Robot.new
+robodir.register(robot)
+generator = -> { 'AA111' }
+robot = Robot.new(name_generator: generator)
+robodir.register(robot)
+puts robodir.directory
